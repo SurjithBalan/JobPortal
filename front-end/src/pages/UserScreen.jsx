7dc5de9e7605e0bb5f50d770f0e8a20930ca635b
Barani203 <naveenkumarmy203@gmail.com>
2025-04-01 17:55:51 +0530

    correction

--- front-end/src/pages/UserScreen.jsx
+++ front-end/src/pages/UserScreen.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import UserNavbar from '../components/usernavbar/UserNavbar'
 import UserBanner from '../components/userbanner/UserBanner'
-import JobCategories from '../components/jobcategory/BrowsCategory'
+import JobCategories from '../components/jobcategory/BrowseCategory'
 import Tophiring from '../components/tophiring/Tophiring'
 function UserScreen() {
   return (
