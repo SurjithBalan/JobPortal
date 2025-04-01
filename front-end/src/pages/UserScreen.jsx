4af2b9ac312ea12a6d1ab11e35a9f08e8d81dac5
Elu620758 <elumalai620758@gmail.com>
2025-04-01 16:46:45 +0530

    browseCatogery updated

--- front-end/src/pages/UserScreen.jsx
+++ front-end/src/pages/UserScreen.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import UserNavbar from '../components/usernavbar/UserNavbar'
 import UserBanner from '../components/userbanner/UserBanner'
-// import JobCategoriesCarousel from '../components/jobcategory/BrowsCategory'
+import JobCategoriesCarousel from '../components/jobcategory/BrowseCategory'
 function UserScreen() {
   return (
     <div>
         <UserNavbar/>
         <UserBanner/>
-       {/* <JobCategoriesCarousel/> */}
+       <JobCategoriesCarousel/>
     </div>
   )
 }
