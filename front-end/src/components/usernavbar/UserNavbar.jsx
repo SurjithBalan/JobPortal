84aaea6dc63de35207ad1913a39345ab9f14fe7b
Barani203 <naveenkumarmy203@gmail.com>
2025-04-16 12:59:39 +0530

    navbar

--- front-end/src/components/usernavbar/UserNavbar.jsx
+++ front-end/src/components/usernavbar/UserNavbar.jsx
@@ -1,7 +1,8 @@
 import React,{useState} from 'react'
 import logo from "../../assets/image/skylarklogo.png";
 import "../usernavbar/usernavbar.css";
-import { HiUser } from "react-icons/hi2";
+import { FaStar } from "react-icons/fa";
+import { PiUserCircleThin } from "react-icons/pi";
 import { MdCorporateFare } from "react-icons/md";
 import CompanyLogin from "../companylogin/CompanyLogin";
 import "../companylogin/companylogin.css";
@@ -28,78 +29,120 @@ function UserNavbar() {
   }
   return (
     <>
-    <nav
-      className="navbar navbar-expand-lg fixed-top "
-      style={{ backgroundColor: "#003366" }}
-    >
-      <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarTogglerDemo03"
-          aria-controls="navbarTogglerDemo03"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <img src={logo} className="image"></img>
-        <div className="collapse navbar-collapse " id="navbarTogglerDemo03">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a
-                className="nav-link active text-white"
-                id="nav-link"
-                aria-current="page"
-                href="#"
-              >
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                Job
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="" className="nav-link text-white ">
-                Contact US
-              </a>
-            </li>
-          </ul>
-          <div className="btn-login ">
-              <a onClick={candidateOpen} className="hover-underline text-white">
-                Candidate Login
-                <span className="m-1">
-                  <HiUser />
-                </span>{" "}
-              </a>
-
-              <span className="text-light mx-2">/</span>
-              <a
+       <nav
+        className="navbar navbar-expand-lg sticky-top "
+        style={{ backgroundColor: "#1E88E5" }}
+      >
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo03 , #navbarNavDropdown"
+            aria-controls="navbarTogglerDemo03,navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <img src={logo} className="image"></img>
+          <div className="collapse navbar-collapse " id="navbarTogglerDemo03">
+            <ul className="navbar-nav m-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active text-white"
+                  id="nav-link"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">
+                  Job
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">
+                  Featured{" "}
+                  <span>
+                    <FaStar color="#FFA900" />
+                  </span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="" className="nav-link text-white ">
+                  Contact US
+                </a>
+              </li>
+            </ul>
+            <div className="btn-login ">
+              <button
                 onClick={companyOpen}
-                className="hover-underline "
-                style={{ color: "#FF7F11" }}
+                className="hover-underline mx-3  "
+               
               >
-                Company Login
-                <span className="m-1">
-                  <MdCorporateFare />
+                <span className="m-2">
+                  <MdCorporateFare fontSize="1.5em" />
                 </span>
-              </a>
-            </div>
-        </div>
-      </div>
-    </nav>
+                Company Login
+              </button>
 
-    <Modal  show={show} onHide={handleClose} animation={false} centered>
-      <Modal.Header  closeButton>
-      </Modal.Header>
-      <Modal.Body>
-        {candidate && <UserLogin />}
-        {company && <CompanyLogin />}
-      </Modal.Body>
-    </Modal>
+              <button
+                onClick={candidateOpen}
+                className="hover-underline  mx-3"
+               
+              >
+                <span className="m-1">
+                  <PiUserCircleThin color="black" fontSize="1.5em" />
+                </span>{" "}
+                Candidate Login
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <div
+            className="collapse navbar-collapse justify-content-center"
+            id="navbarNavDropdown"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-items">
+                <a className="nav-link  " aria-current="page" href="#">
+                  Featured Hiring
+                </a>
+              </li>
+              <li className="nav-items">
+                <a className="nav-link" href="#">
+                  Resume AI
+                </a>
+              </li>
+              <li className="nav-items">
+                <a className="nav-link" href="#">
+                  Interview Tips
+                </a>
+              </li>
+              <li className="nav-items">
+                <a className="nav-link" href="#">
+                  Analytics
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Modal show={show} onHide={handleClose} animation={false} centered>
+        <Modal.Header closeButton></Modal.Header>
+        <Modal.Body>
+          {candidate && <UserLogin />}
+          {company && <CompanyLogin />}
+        </Modal.Body>
+      </Modal>
   </>
   )
 }
